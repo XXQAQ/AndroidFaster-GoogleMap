e487cc491ae0b6983b9149f860b18d1056b9d2b1
XQ <120363542@qq.com>
2018-12-30 22:39:49 +0800

    使用最新定位方式

--- library/src/main/java/com/xq/androidfaster_map/service/LocationService.java
+++ library/src/main/java/com/xq/androidfaster_map/service/LocationService.java
@@ -4,12 +4,16 @@ import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
-import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v4.content.LocalBroadcastManager;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.xq.androidfaster.util.tools.BundleUtil;
-import com.xq.androidfaster.util.tools.LocationUtils;
 
 public class LocationService extends Service {
 
@@ -21,39 +25,44 @@ public class LocationService extends Service {
         return null;
     }
 
+    protected FusedLocationProviderClient locationClient;
+    protected LocationCallback locationCallback;
+
     @SuppressLint("MissingPermission")
     @Override
     public void onCreate() {
         super.onCreate();
-        LocationUtils.register(1000, 0, new LocationUtils.OnLocationChangeListener() {
+
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000); // two minute interval
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        locationCallback = new LocationCallback(){
             @Override
-            public void getLastKnownLocation(Location location) {
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                Location location = locationResult.getLastLocation();
+
                 Intent intent = new Intent();
                 intent.setAction(ACTION_LOCATION);
                 intent.putExtras(new BundleUtil.Builder().putParcelable(BundleUtil.KEY_DATA,location).build());
                 LocalBroadcastManager.getInstance(getBaseContext()).sendBroadcast(intent) ;
             }
+        };
 
-            @Override
-            public void onLocationChanged(Location location) {
-                Intent intent = new Intent();
-                intent.setAction(ACTION_LOCATION);
-                intent.putExtras(new BundleUtil.Builder().putParcelable(BundleUtil.KEY_DATA,location).build());
-                LocalBroadcastManager.getInstance(getBaseContext()).sendBroadcast(intent) ;
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-        });
+        locationClient.requestLocationUpdates(locationRequest,locationCallback,Looper.myLooper());
     }
 
     @SuppressLint("MissingPermission")
     @Override
     public void onDestroy() {
         super.onDestroy();
-        LocationUtils.unregister();
+        locationClient.removeLocationUpdates(locationCallback);
+        locationCallback=null;
     }
 
 }
